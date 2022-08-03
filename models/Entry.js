61537c1712571609dc9a6a94db3efa96b476bb10
issertmg <issertmg@gmail.com>
2022-08-03 16:49:39 +0800

    Added validation to entry schema

--- models/Entry.js
+++ models/Entry.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const entrySchema = new mongoose.Schema({
     datecreated: {type: Date, default: Date.now},
-    mood: {type: String, default: ''},
-    note: {type: String, default: ''}
+    mood: {type: String, required: true},
+    note: {type: String, required: true}
 })
 
 module.exports = mongoose.model('Entry', entrySchema, 'Entries');
